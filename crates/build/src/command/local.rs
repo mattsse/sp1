d9146a9853b773413f599164b095d7478534d225
Matthias Seitz <matthias.seitz@outlook.de>
2025-01-23 00:33:22 +0100

    chore: include rustc --version stderr in panic

--- crates/build/src/command/local.rs
+++ crates/build/src/command/local.rs
@@ -4,8 +4,10 @@ use crate::{BuildArgs, HELPER_TARGET_SUBDIR};
 use cargo_metadata::camino::Utf8PathBuf;
 use dirs::home_dir;
 
-use super::utils::{get_program_build_args, get_rust_compiler_flags};
-use super::TOOLCHAIN_NAME;
+use super::{
+    utils::{get_program_build_args, get_rust_compiler_flags},
+    TOOLCHAIN_NAME,
+};
 
 /// Get the command to build the program locally.
 pub(crate) fn create_local_command(
@@ -36,7 +38,11 @@ pub(crate) fn create_local_command(
             .expect("rustc --version should succeed");
 
         if !output.status.success() {
-            panic!("Failed to run rustc --version {:?}", String::from_utf8_lossy(&output.stdout));
+            panic!(
+                "Failed to run rustc --version {:?}\n{:?}",
+                String::from_utf8_lossy(&output.stdout),
+                String::from_utf8_lossy(&output.stderr)
+            );
         }
 
         let stdout_string =
